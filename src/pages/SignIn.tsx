accc989e396f60f1659afbc4edfc1185afe998b8
lgu0521 <lgu0521@gmail.com>
2022-02-03 22:51:16 +0900

    login antdesign icon test

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,12 @@
 import { Form, Input, Button } from 'antd';
 import axios from 'axios';
+import { UserOutlined, UnlockOutlined } from '@ant-design/icons';
+
 const SignIn = () => {
 
     const onFinish = async (values: any) => {
         try {
-            const res = await axios.post(process.env.REACT_APP_API_URL+'auth/signin',values);
+            const res = await axios.post(process.env.REACT_APP_API_URL + 'auth/signin', values);
             console.log(res);
         } catch {
 
@@ -16,36 +18,39 @@ const SignIn = () => {
     };
 
     return (
-        <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="ID/EMAIL"
-                name="email"
-                rules={[{ required: true, message: '이메일을 입력해 주세요' }]}
+        <>
+            <h1><strong>GrounDI</strong></h1>
+            <h1>내 소중한 공간을, 공유하다</h1>
+            <h3>웰니스 공간대여 그라운디</h3>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <Input />
-            </Form.Item>
+                <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: '이메일을 입력해 주세요' }]}
+                >
+                    <Input size="large" placeholder="이메일" prefix={<UserOutlined />} />
+                </Form.Item>
 
-            <Form.Item
-                label="비밀번호"
-                name="password"
-                rules={[{ required: true, message: '비밀번호를 입력해 주세요' }]}
-            >
-                <Input.Password />
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                    로그인
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: '비밀번호' }]}
+                >
+                    <Input.Password size="large" placeholder="비밀번호" prefix={<UnlockOutlined />} />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        로그인
           </Button>
-            </Form.Item>
-        </Form>
+                </Form.Item>
+            </Form>
+        </>
     )
 }
 
