b39bbe1cfa7abec1d79183744b08d639a7caac07
lgu0521 <lgu0521@gmail.com>
2022-02-14 17:33:47 +0900

    center create api 연결

--- src/pages/center/new/index.tsx
+++ src/pages/center/new/index.tsx
@@ -1,11 +1,14 @@
 import { useForm } from "react-hook-form";
+import { client } from "services/axios";
+import styled from "styled-components";
 
 
 const CenterNew = () => {
     const { register, handleSubmit } = useForm();
 
-    const onSubmit = (data: any) => {
-        console.log(data);
+    const onSubmit = async (data: any) => {
+        const res = await client.post(process.env.REACT_APP_API_URL + 'center/create', data);
+        console.log(res);
     }
 
     return (
@@ -14,32 +17,26 @@ const CenterNew = () => {
                 <h1>또 만나게 되어 반가워요. 오늘 멋지네요!</h1>
                 <h5>아래에서 워크스페이스를 선택하여 팀과 계속 협업하세요.</h5>
             </div>
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register('owner')} />
-                    <input {...register('name')} />
-                    <input {...register('type')} />
-                    <input {...register('type')} />
-                    <input type="file" {...register('file')} />
 
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Wrap>
+                    <label>센터이름을 입력해주세요</label>
+                    <input {...register('name')} />
                     <label>전화번호를 입력해주세요</label>
                     <input {...register('phoneNumber')} />
                     <label>주소를 입력해주세요</label>
-                    <input {...register('location')} />
-                    <label>우리 업체 위치를 확인해주세요</label>
-                    <input {...register('lat, long')} />
-                    <label>찾아오는 길을 설명해주세요</label>
-                    <input {...register('description')} />
-                    <label>관리자 전화번호</label>
-                    <input {...register('adminnumber')} />
-                    <label>관리자 이메일</label>
-                    <input {...register('email')} />
-
+                    <input {...register('address')} />
                     <button type="submit">제출</button>
-                </form >
-            </div>
+                </Wrap>
+            </form >
+
         </>
     )
 }
 
+const Wrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+`
 export default CenterNew;
